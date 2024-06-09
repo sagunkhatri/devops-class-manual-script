0fd4953510b60f551c54bddb2e5d66ca770b2328
Sagun Khatri <sagunkhatri@duck.com>
2024-06-09 18:26:43 +0545

    add: reboot command

--- initial-setup.sh
+++ initial-setup.sh
@@ -5,4 +5,9 @@
 # Update System
 sudo apt update
 sudo apt upgrade -y
-sudo apt install bash tmux git mysql-server nodejs npm nginx neovim zsh -y
+sudo apt install bash tmux git mysql-server nodejs npm nginx neovim bash-completion -y
+sudo apt-get install
+
+# Uncomment reboot command below
+
+# sudo reboot
